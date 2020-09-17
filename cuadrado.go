f4eca562e9fac75bbba275fa4c541a45c7d2356d
César Tonatiuh Tamayo Ávila <cesar.tamayo@alumnos.udg.mx>
2020-09-17 00:25:49 -0500

    Podemos leer el cuadrado

---
+++ cuadrado.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+
+// 	- Calcular el área del cuadrado
+	var lado float64
+
+	fmt.Println("Lado del cuadrado: ")
+	fmt.Scan(&lado)
+
+	fmt.Println("El lado es:",lado, "cm" )
+
+
+}
