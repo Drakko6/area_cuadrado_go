940de22a4d7b307c59d497ebc5a62c99b862319f
César Tonatiuh Tamayo Ávila <cesar.tamayo@alumnos.udg.mx>
2020-09-17 00:28:13 -0500

    Se cambio Println por Print al escanear

--- cuadrado.go
+++ cuadrado.go
@@ -7,7 +7,7 @@ func main(){
 // 	- Calcular el área del cuadrado
 	var lado float64
 
-	fmt.Println("Lado del cuadrado: ")
+	fmt.Print("Lado del cuadrado: ")
 	fmt.Scan(&lado)
 
 	fmt.Println("El lado es:",lado, "cm" )
