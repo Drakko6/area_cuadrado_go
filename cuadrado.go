b180ba32f2057bb0339720848ff505039e4393b9
César Tonatiuh Tamayo Ávila <cesar.tamayo@alumnos.udg.mx>
2020-09-23 21:28:13 -0500

    Creamos funcion suma

--- cuadrado.go
+++ cuadrado.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+
+func suma(a int, b int) int {
+	
+	return a + b
+}
+
+
+
+
 func main(){
 
 // 	- Calcular el área del cuadrado
@@ -15,5 +24,8 @@ func main(){
 	fmt.Println("El lado es:",lado, "cm" )
 	fmt.Println("El área es:", area, "cm2")
 
+
+	a := suma(2, 3)
+	fmt.Println("La suma de numeros es:", a )
 
 }
